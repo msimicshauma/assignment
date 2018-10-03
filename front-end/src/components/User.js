233740fd4250a645e6222dc3f67fdcce416bd403
msimicshauma <milos.simic95@gmail.com>
2018-10-03 20:47:09 +0200

    added suggestions for users

--- front-end/src/components/User.js
+++ front-end/src/components/User.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import UserFriends from './Friends/UserFriends';
+import Suggested from './Friends/Suggested';
 
 class User extends Component {
   state = {
@@ -26,22 +27,28 @@ class User extends Component {
   render() {
     const user = this.state.user ? (
       <div className="user">
-        <h1>
-          <span className="property">Name: </span>
-          {this.state.user.firstName + ' ' + this.state.user.surname}
-        </h1>
-        <p>
-          <span className="property">Age: </span>
-          {this.state.user.age}
-        </p>
-        <p>
-          <span className="property">Gender: </span>
-          {this.state.user.gender}
-        </p>
-        <p>
-          <span className="property">Friends: </span>
-          {this.state.user.friends.length}
-        </p>
+        <div className="user-info-container">
+          <div className="user">
+            <h1>
+              <span className="property">Name: </span>
+              {this.state.user.firstName + ' ' + this.state.user.surname}
+            </h1>
+            <p>
+              <span className="property">Age: </span>
+              {this.state.user.age}
+            </p>
+            <p>
+              <span className="property">Gender: </span>
+              {this.state.user.gender}
+            </p>
+            <p>
+              <span className="property">Friends: </span>
+              {this.state.user.friends.length}
+            </p>
+          </div>
+          <Suggested friends={this.state.user.friends} id={this.props.match.params.user_id} />
+        </div>
+        <h1>FRIENDS</h1>
         <UserFriends friends={this.state.user.friends} />
       </div>
     ) : (
